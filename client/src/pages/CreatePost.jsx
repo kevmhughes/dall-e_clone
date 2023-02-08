000b5a7352529bcdaaa9963f8adbffefd8cb4966
Kevin Hughes <48656356+kevmhughes@users.noreply.github.com>
2023-02-08 02:37:43 +0100

    Update

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -49,7 +49,7 @@ const CreatePost = () => {
     if(form.prompt && form.photo) {
       setLoading(true);
       try {
-        const response = await fetch("https://dall-e-clone-xcf1.onrender.com/api/v1/post", {
+        const response = await fetch("/api/v1/post", {
           method: "POST",
           headers: {
             'Content-Type': 'application/json'
